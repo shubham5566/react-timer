33795b964a2214938bdf41eca2c306cd917cf27c
shubham b <shubhambhavsar5566@gmail.com>
2024-03-09 14:43:15 +0530

    react counter inial setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [day, setDay] = useState(0)
+  const [hours, setHours] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [sec, setSec] = useState(0)
+
+  return (
+    <>
+   
+    </>
+  )
+}
+
+export default App
